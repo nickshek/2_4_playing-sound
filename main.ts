09e4267abb3e0c7e5f38ab77189b88baac64c4bc
nickshek <nickshek@users.noreply.github.com>
2021-10-09 16:46:35 +0800

    playsound

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Playing Sound")
+    music.startMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Once)
+})
 basic.showString("READY")
 basic.forever(function () {
 	
